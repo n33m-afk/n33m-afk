5f829f25cfcfbad3c9f028e605cd2daeaa459ced
n33m-afk <1211110755@student.mmu.edu.my>
2025-06-25 18:55:50 +0800

    Update LoadingBar.js

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -1,5 +1,5 @@
-class LoadingBar{
-	constructor(options){
+class LoadingBar {
+	constructor(options) {
 		this.domElement = document.createElement("div");
 		this.domElement.style.position = 'fixed';
 		this.domElement.style.top = '0';
@@ -12,6 +12,7 @@ class LoadingBar{
 		this.domElement.style.alignItems = 'center';
 		this.domElement.style.justifyContent = 'center';
 		this.domElement.style.zIndex = '1111';
+
 		const barBase = document.createElement("div");
 		barBase.style.background = '#aaa';
 		barBase.style.width = '50%';
@@ -19,34 +20,26 @@ class LoadingBar{
 		barBase.style.borderRadius = '10px';
 		barBase.style.height = '15px';
 		this.domElement.appendChild(barBase);
+
 		const bar = document.createElement("div");
-		bar.style.background = '#22a';
-		bar.style.width = '50%';
+		bar.style.background = '#800080'; // Purple color
+		bar.style.width = '0';
 		bar.style.borderRadius = '10px';
 		bar.style.height = '100%';
-		bar.style.width = '0';
 		barBase.appendChild(bar);
+
 		this.progressBar = bar;
-		
+
 		document.body.appendChild(this.domElement);
-		
-		function onprogress(delta){
-			const progress = delta*100;
-			loader.progressBar.style.width = `${progress}%`;
-		}
 	}
-	
-	set progress(delta){
-		const percent = delta*100;
+
+	set progress(delta) {
+		const percent = delta * 100;
 		this.progressBar.style.width = `${percent}%`;
 	}
-	
-	set visible(value){
-		if (value){
-			this.domElement.style.display = 'flex';
-		}else{
-			this.domElement.style.display = 'none';
-		}
+
+	set visible(value) {
+		this.domElement.style.display = value ? 'flex' : 'none';
 	}
 }
 
